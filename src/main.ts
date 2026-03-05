344ae2b706bad38b4521482b1d3f01c221076744
Guillaume <guillaume.fontaine.ab@gmail.com>
2026-03-05 11:55:28 +0100

    route singin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieSession from "cookie-session";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['esgi']
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,12 @@
 import {
-  Controller,
-  Post,
-  Body,
-  Param,
-  Get,
-  Query,
-  NotFoundException,
-  UseInterceptors,
+    Controller,
+    Post,
+    Body,
+    Param,
+    Get,
+    Query,
+    NotFoundException,
+    UseInterceptors, Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
@@ -17,39 +17,45 @@ import { SerializeInterceptor } from '../interceptors/dto-serialize.interceptor'
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('signup')
+  @Get('/whoami')
   @UseInterceptors(new SerializeInterceptor(UserResponseDto))
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.signup(
-      createUserDto.email,
-      createUserDto.password,
-    );
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
   }
 
-  @Post('signin')
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Post('/signup')
   @UseInterceptors(new SerializeInterceptor(UserResponseDto))
-  signin(@Body() createUserDto: CreateUserDto) {
-      return this.usersService.signin(
-      createUserDto.email,
-      createUserDto.password,
-    );
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.usersService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  @UseInterceptors(new SerializeInterceptor(UserResponseDto))
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.usersService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
   @UseInterceptors(new SerializeInterceptor(UserResponseDto))
-  async findById(@Param('id') id: string) {
+  async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
-    console.log(user);
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('user not found');
     }
     return user;
   }
 
   @Get()
-  findUserByEmail(@Query('email') email: string) {
+  findAllUsers(@Query('email') email: string) {
     return this.usersService.findByEmail(email);
   }
-
-
 }
